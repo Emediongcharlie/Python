66baf811c87c7ae8ab1a7693cb1b9ce77bcbee43
Emediong Charlie <emediongcharliegmail.com>
2024-04-24 12:47:14 +0100

    Chapter 3 exercise

---
+++ Chapter3 Assignments/table_of_square_and_cubes.py
@@ -0,0 +1,8 @@
+number = 0
+square = 0
+cube = 0
+for number in range(1):
+	print(f' number \t square \t cube')
+for index in range(6):
+	print(f'{number} \t\t {number**2} \t\t {number**3}')
+	number = number + 1
