e4316891de80ce002ddb6bcde9d7d3aef50e4522
Emediong Charlie <emediongcharliegmail.com>
2024-04-24 12:49:13 +0100

    chapter 3

---
+++ Chapter3 Assignments/turing_test.py
@@ -0,0 +1,10 @@
+
+
+problem = input("What is your problem?")
+
+problem_before = input("Have you ad this problem bfere(yes or no)?")
+
+if(problem_before == "yes"):
+	print("Well, you have it again")
+if(problem_before == "no"):
+	print("Well, you have it now")
